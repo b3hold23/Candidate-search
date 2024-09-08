7edbf130b3c014e904366f2f6de06ce6515d7441
unknown <rosa.angel.daniel@gmail.com>
2024-09-07 18:04:54 -0600

    added nav bar to site

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,19 @@
+// TODO: Add necessary code to display the navigation bar and link between the page
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav>
+
+      <ul className="nav">
+        <li className="nav-item">
+          <a className="nav-link active" href="/CandidateSearch">Home</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link active" href="/SavedCandidates">Saved Candidates</a>
+        </li>
+      </ul>
+
+    </nav>
   )
 };
 
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+// import { useState, useEffect } from 'react';
+// import { searchGithub, searchGithubUser } from '../api/API';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  return <h1>Candidate Search</h1>;
 };
 
 export default CandidateSearch;
